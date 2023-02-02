8627dac1bf487dc35312a112b2bc7156d3c3def8
caobandoc <obandocarlosandres@gmail.com>
2023-02-02 11:05:17 -0500

    Formularios

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,12 +2,93 @@ function Formulario() {
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Segumiento Pacientes</h2>
-      <p className="text-lg mt-5 text-center">
-        Añade Pacientes y {''}
+
+      <p className="text-lg mt-5 text-center mb-10">
+        Añade Pacientes y {""}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form></form>
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        <div className="mb-5">
+          <label
+            htmlFor="mascota"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre Mascota
+          </label>
+          <input
+            id="mascota"
+            type="text"
+            placeholder="Nombre de la Mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="dueño"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre del dueño
+          </label>
+          <input
+            id="dueño"
+            type="text"
+            placeholder="Nombre del dueño"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Correo del dueño"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="alta"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Alta
+          </label>
+          <input
+            id="alta"
+            type="date"
+            placeholder="Correo del dueño"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="sintomas"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Sintomas
+          </label>
+          <textarea
+            id="sintomas"
+            placeholder="Sintomas del paciente"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
+          value="Agregar paciente"
+        />
+      </form>
     </div>
   );
 }
