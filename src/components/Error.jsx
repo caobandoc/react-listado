c446c29660587b4dcdbde704a7f16cfdecfd316a
caobandoc <obandocarlosandres@gmail.com>
2023-02-03 11:36:31 -0500

    prop children

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
-const Error = ({mensaje}) => {
+const Error = ({children}) => {
   return (
     <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5 uppercase">
-      <p>{mensaje}</p>
+      {children}
     </div>
   );
 };
